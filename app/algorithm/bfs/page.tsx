b3c17b51a718b920004d29ba4e1ce60895a6e96b
choikiyeop <cky9709@naver.com>
2024-02-20 18:40:40 +0900

    dfs, bfs 구조

---
+++ app/algorithm/bfs/page.tsx
@@ -0,0 +1,11 @@
+export default function BfsPage() {
+  return <main>asd</main>;
+}
+
+/**
+ * 너비우선탐색
+ *
+ * @param
+ * @returns
+ */
+const BFS = () => {};
---
+++ app/algorithm/dfs/page.tsx
@@ -0,0 +1,11 @@
+export default function DfsPage() {
+  return <main>DFS</main>;
+}
+
+/**
+ * 깊이 우선 탐색
+ * 그래프 전체를 탐색하는 알고리즘으로서 깊이를 우선해서 탐색
+ * @param
+ * @returns
+ */
+const DFS = () => {};
