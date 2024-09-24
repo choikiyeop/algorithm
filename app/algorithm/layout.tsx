185e32fb80f9215dfab3448c2dbbc833f4fa0e8b
choikiyeop <cky9709@naver.com>
2024-09-24 19:24:07 +0900

    design: 디자인 수정

--- app/algorithm/layout.tsx
+++ app/algorithm/layout.tsx
@@ -2,9 +2,9 @@ import { ReactNode } from "react";
 
 export default function AlgorithmLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
-      <h1 className="flex justify-center">알고리즘 정리</h1>
-      <div className="mx-auto w-1/3 my-32">{children}</div>
+    <div className="my-16">
+      <h1 className="flex justify-center text-lg font-bold">알고리즘 정리</h1>
+      <div className="mx-auto w-1/3 mt-16">{children}</div>
     </div>
   );
 }
--- app/algorithm/page.tsx
+++ app/algorithm/page.tsx
@@ -1,32 +1,111 @@
 import Link from "next/link";
 
 export default function AlgorithmPage() {
-  const a = true;
-  Number(a);
   return (
-    <main className="container mx-auto">
-      <Link href={"/algorithm/dfs"}>DFS</Link>
-      <Link href={"/algorithm/bfs"}>BFS: 가중치 없는 최단거리</Link>
-      <Link href={"/algorithm/dijkstra"}>다익스트라: 모든 가중치 양수</Link>
-      <Link href={"/algorithm/floyd-warshall"}>
-        플로이드-워셜: 모든 정점과 정점 사이의 최단 경로
-      </Link>
-      <Link href={"/algorithm/prefix-sum"}>누적합</Link>
-      <Link href={"/algorithm/dp"}>DP</Link>
+    <main className="container mx-auto font-sans">
+      <ul className="space-y-3 list-decimal">
+        <li>
+          <Link
+            href={"/algorithm/dfs"}
+            className="flex space-x-2 hover:text-blue-600"
+          >
+            <h2 className="font-semibold">DFS</h2>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/algorithm/bfs"}
+            className="flex space-x-2 hover:text-blue-600"
+          >
+            <h2 className="font-semibold">BFS:</h2>
+            <p>가중치 없는 최단거리</p>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/algorithm/dijkstra"}
+            className="flex space-x-2 hover:text-blue-600"
+          >
+            <h2 className="font-semibold">다익스트라: </h2>
+            <p>모든 가중치 양수</p>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/algorithm/prefix-sum"}
+            className="flex space-x-2 hover:text-blue-600"
+          >
+            <h2 className="font-semibold">누적합</h2>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/algorithm/dp"}
+            className="flex space-x-2 hover:text-blue-600"
+          >
+            <h2 className="font-semibold">DP</h2>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/algorithm/greedy"}
+            className="flex space-x-2 hover:text-blue-600"
+          >
+            <h2 className="font-semibold">탐욕법(Greedy)</h2>
+            <p></p>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/algorithm/binary-search"}
+            className="flex space-x-2 hover:text-blue-600"
+          >
+            <h2 className="font-semibold">이진탐색</h2>
+            <p></p>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/algorithm/back-tracking"}
+            className="flex space-x-2 hover:text-blue-600"
+          >
+            <h2 className="font-semibold">백트래킹</h2>
+            <p></p>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/algorithm/two-pointers"}
+            className="flex space-x-2 hover:text-blue-600"
+          >
+            <h2 className="font-semibold">투포인터</h2>
+            <p></p>
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            href={"/algorithm/floyd-warshall"}
+            className="flex space-x-2 hover:text-blue-600"
+          >
+            <h2 className="font-semibold">플로이드-워셜:</h2>{" "}
+            <p>모든 정점과 정점 사이의 최단 경로</p>
+          </Link>
+        </li>
+      </ul>
+
       <div>구현</div>
       <div>그래프이론</div>
       <div>트리순회</div>
       <div>완탐</div>
-      <div>백트래킹</div>
       <div>비트마스킹</div>
-      <div>그리디</div>
       <div>라인스위핑</div>
-      <div>투포인터</div>
       <div>LIS</div>
       <div>이분탐색</div>
-      <div>최단거리</div>
       <div>팬윅트리</div>
       <div>벨만-포드: 음수 가중치 있음</div>
+      <div>최단거리</div>
+      <div>정렬(거품정렬, 병합정렬, 삽입정렬, 선택정렬, 퀵정렬, 힙정렬)</div>
     </main>
   );
 }
